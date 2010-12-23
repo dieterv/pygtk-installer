f35daa87e1d921fbe19cf4a7ea46b98baab591d4
Dieter Verfaillie <dieterv@optionexplicit.be>
2010-12-23 23:10:45 +0100

    The pygtk_setup_config_files.py script could be useful even after installation, so intall it to the Scripts dir.

--- wix/binary/pygtk_setup_config_files.py
+++ wix/binary/pygtk_setup_config_files.py
@@ -6,21 +6,32 @@ import sys
 import subprocess
 
 
-TARGETDIR = os.path.abspath(sys.argv[1].strip())
-TMPDIR = os.path.abspath(sys.argv[2].strip())
+if len(sys.argv) == 3:
+    # Run from the all-in-one installer
+    TARGETDIR = os.path.abspath(sys.argv[1].strip())
+    TMPDIR = os.path.abspath(sys.argv[2].strip())
+
+    # We'll redirect output to a logfile
+    sys.stdout = open(os.path.join(TMPDIR, 'pygtk_setup_config_files.log'), 'w')
+    sys.stderr = open(os.path.join(TMPDIR, 'pygtk_setup_config_files.log'), 'w')
+
+    # And need to make sure Popen calls point stderr to subprocess.PIPE or
+    # suffer ugly popup "Command Prompts" during install...
+    stderr = subprocess.PIPE
+else:
+    # Run from the Scripts directory
+    TARGETDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+    stderr = sys.stderr
 
 
 def redirect(executable, configile):
     output=open(configfile, 'w')
-    subprocess.Popen(executable, shell=True, stdout=output, stderr=subprocess.PIPE)
+    subprocess.Popen(executable, shell=True, stdout=output, stderr=stderr)
     output.close()
 
 
 if __name__ == '__main__':
-    logfile = open(os.path.join(TMPDIR, 'pygtk_setup_config_files.log'), 'w')
-    sys.stdout = logfile
-    sys.stdout = logfile
-
     # gdk-pixbuf-query-loaders
     executable = os.path.join(TARGETDIR, 'Lib', 'site-packages', 'gtk-2.0',
                               'runtime', 'bin', 'gdk-pixbuf-query-loaders.exe')
